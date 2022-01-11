4aa757f0e269e862d81c67b0ac7005a3c43f295e
Egor Potebenko <hitechnic.uu68@gmail.com>
2022-01-11 22:48:24 +0200

    Adding smooth scrolling by clicking on header links, adding function closing burger menu by clicking on burger menu links.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// Function for inverting custom image to background image
 function ibg() {
   let ibg = document.querySelectorAll(".ibg");
   for (var i = 0; i < ibg.length; i++) {
@@ -10,9 +11,56 @@ function ibg() {
 
 ibg();
 
+// Smooth scrolling to anchors
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger, .header__nav").toggleClass("active");
     $("body").toggleClass("lock");
   });
 });
+
+// Closing burger menu by clicking on the links
+$(document).ready(function () {
+  $(".header__link").click(function (event) {
+    $(".header__burger, .header__nav").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+
+// Scroll to anchors
+(function () {
+  const smoothScroll = function (targetEl, duration) {
+    const headerElHeight = document.querySelector(".header").clientHeight;
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.pageYOffset;
+    let startTime = null;
+
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
+  };
+
+  const scrollTo = function () {
+    const links = document.querySelectorAll(".js-scroll");
+    links.forEach((each) => {
+      each.addEventListener("click", function () {
+        const currentTarget = this.getAttribute("href");
+        smoothScroll(currentTarget, 1000);
+      });
+    });
+  };
+  scrollTo();
+})();
