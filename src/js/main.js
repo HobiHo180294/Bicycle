5719c2f17cc1b2ba5c20d886c1a3f79edf009eb5
Egor Potebenko <hitechnic.uu68@gmail.com>
2022-01-11 22:29:14 +0200

    Creating adaptive layout for mobile, laptop and desktop. Fixed bugs with grid demo on different devices.

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,10 @@ function ibg() {
 }
 
 ibg();
+
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger, .header__nav").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
